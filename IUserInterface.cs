95dfc8d2c6bf7e7df382497a892ebcb16ea90d8e
David-Langat <davidcheruiyot.langat@connect.qut.edu.au>
2025-07-01 17:35:47 +1000

    Created 'IUserInterface' interface to act as a contract for any user interface defining what a UI need to do e.g display welcome message , decide game to play. This made the previous 'UserInterface' class obsolete.

---
+++ IUserInterface.cs
@@ -0,0 +1,17 @@
+
+public interface IUserInterface
+{
+    void DisplayWelcomeMessage();
+    void DecideGameToPlay();
+    void DecidePlayerMode();
+    void InvalidInput();
+    void RequestPlayerNameInput();
+    string GetPlayerName();
+    void ShowPlayerTurn(Player player);
+    void SOSHelpGuide();
+    void ConnectFourHelpGuide();
+    int GetIntUserInput();
+    void DisplayBoard(string board);
+    void ShowWinner(string winner);
+    (int, int, string) GetPlayerMove(Player player);
+}
